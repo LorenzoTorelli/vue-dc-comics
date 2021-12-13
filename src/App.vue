8c1ca405d5cad357ca9a4bba04582744e4b0f491
LorenzoTorelli <lorenzo.torelli98@gmail.com>
2021-12-13 15:17:34 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Header/>
+    <Main/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Main,
+    Footer,
+
+  }
+}
+</script>
+
+<style lang="scss">
+@import url('./assets/style/global.scss');
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="header">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style>
+    #header {
+        width: 100vw;
+        height: 100px;
+        background-color: blue;
+    }
+</style>
