dc0dc1c60101e877bc788f2edae03f100741f68c
LorenzoTorelli <lorenzo.torelli98@gmail.com>
2021-12-13 18:27:14 +0100

    header-dinamico

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,16 +5,7 @@
             <div id="nav-box">
                 <nav>
                     <ul>
-                        <li><a href="">characters</a></li>
-                        <li><a href="">comics</a></li>
-                        <li><a href="">movies</a></li>
-                        <li><a href="">tv</a></li>
-                        <li><a href="">games</a></li>
-                        <li><a href="">collectibles</a></li>
-                        <li><a href="">videos</a></li>
-                        <li><a href="">fans</a></li>
-                        <li><a href="">news</a></li>
-                        <li><a href="">shop</a></li>
+                        <li v-for= " (elemento,index) in menu" :key= "index"><a href="">{{elemento.nome}}</a></li>
                     </ul>
                 </nav>
             </div>
@@ -25,6 +16,42 @@
 <script>
 export default {
     name: 'Header',
+    data() {
+        return {
+            menu: [
+            {
+                nome:'characters'
+            },
+            {
+                nome:'comics'
+            },
+             {
+                nome:'movies'
+            },
+             {
+                nome:'tv'
+            },
+             {
+                nome:'games'
+            },
+             {
+                nome:'collectibles'
+            },
+             {
+                nome:'videos'
+            },
+             {
+                nome:'fans'
+            },
+             {
+                nome:'news'
+            },
+             {
+                nome:'shop'
+            },
+            ],
+        }
+    },
 }
 </script>
 
